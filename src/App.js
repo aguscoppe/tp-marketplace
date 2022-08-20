99a8a477e4861ea48c702bf42ad3be3df17a9800
fdiazTAG <fdiaz@amherst.com>
2022-08-20 17:18:30 -0300

    added /login. installed @mui/icons-material

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import CourseDetail from "./pages/CourseDetail";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import "./style.css";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
       <Route path="/class">
         <Route path=":id" element={<CourseDetail />} />
       </Route>
       {/* create page + replace Home*/}
-      <Route path="/login" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Home />} />
       <Route path="/reset-password" element={<Home />} />
     </Routes>
