830743a606ec6b455d206e1c2b59974c591264d3
fdiazTAG <fdiaz@amherst.com>
2022-09-16 17:06:31 -0300

    Estilos para el /about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,18 @@
-import NavBar from '../components/NavBar';
-import { Grid, Typography } from '@mui/material';
+import NavBar from "../components/NavBar";
+import { Box, Grid, Typography } from "@mui/material";
 
 const styles = {
-  marginTop: '50px',
+  marginLeft: "150px",
+  width: "35vw",
+  minHeight: "500px",
+  backgroundColor: "#FFF",
+  borderRadius: "30px",
   h5: {
-    textAlign: 'center',
+    textAlign: "center",
   },
-  '& .MuiTypography-root': {
-    fontFamily: 'Montserrat',
-    margin: '12px',
+  "& .MuiTypography-root": {
+    fontFamily: "Montserrat",
+    margin: "12px",
   },
 };
 
@@ -16,24 +20,50 @@ const About = ({ currentUser }) => {
   return (
     <>
       <NavBar currentUser={currentUser} />
-      <Grid container justifyContent='center' sx={styles}>
-        <Grid item xs={11} lg={10}>
-          <Typography variant='h5'>¿Para qué sirve nuestro portal?</Typography>
-          <Typography variant='body1'>
-            Somos una plataforma de aprendizaje que conecta alumnos y
-            profesores. Un profesor puede crear, editar y eliminar clases. Un
-            alumno puede inscribirse a clases, comentarlas, calificarlas y leer
-            comentarios de otros alumnos. La búsqueda avanzada permite filtrar
-            clases por nombre, tipo de clase, materia o frecuencia.
-          </Typography>
-          <Typography variant='h5'>¿Cómo se usa nuestro portal?</Typography>
-          <Typography variant='body1'>
-            Para poder comentar o calificar una clase, el alumno deberá estar
-            inscripto a ella y la clase debe estar en estado Aceptado o
-            Finalizado.
-          </Typography>
+      <Box
+        sx={{
+          boxShadow: 20,
+          display: "flex",
+          width: "100%",
+          justifyContent: "start",
+          alignItems: "center",
+          background:
+            'url("https://sephorconsulting.es/kitdigital/wp-content/uploads/2022/01/mujer-ordenador.png"), #1976d2',
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPositionX: "right",
+          height: "90.5vh",
+        }}
+      >
+        <Grid container justifyContent="center" sx={styles}>
+          <Grid item xs={11} lg={10}>
+            <Box sx={{ marginTop: "50px" }}>
+              <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                ¿Para qué sirve nuestro portal?
+              </Typography>
+              <Typography variant="body1">
+                Somos una plataforma de aprendizaje que conecta alumnos y
+                profesores. Un profesor puede crear, editar y eliminar clases.
+                Un alumno puede inscribirse a clases, comentarlas, calificarlas
+                y leer comentarios de otros alumnos. La búsqueda avanzada
+                permite filtrar clases por nombre, tipo de clase, materia o
+                frecuencia.
+              </Typography>
+            </Box>
+            <Box sx={{ marginTop: "50px" }}>
+              <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                ¿Cómo se usa nuestro portal?
+              </Typography>
+
+              <Typography variant="body1">
+                Para poder comentar o calificar una clase, el alumno deberá
+                estar inscripto a ella y la clase debe estar en estado Aceptado
+                o Finalizado.
+              </Typography>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 };
